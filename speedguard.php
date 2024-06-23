440a5e43fa4a1ded7313cfeab8c60d69b6543dc7
Sabrina Zeidan <sabrinazeidan@gmail.com>
2024-06-23 20:23:48 +0200

    Remove freeemius from free version

--- speedguard.php
+++ speedguard.php
@@ -27,207 +27,158 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-if ( function_exists( 'speedguard_fs' ) ) {
-	speedguard_fs()->set_basename( true, __FILE__ );
-} else {
-	// DO NOT REMOVE THIS IF, IT IS ESSENTIAL FOR THE `function_exists` CALL ABOVE TO PROPERLY WORK.
-	if ( ! function_exists( 'speedguard_fs' ) ) {
+// SpeedGuard main file logic ...
 
-		// ... Freemius integration snippet ...
-		if ( ! function_exists( 'speedguard_fs' ) ) {
-			// Create a helper function for easy SDK access.
-			function speedguard_fs() {
-				global $speedguard_fs;
-
-				if ( ! isset( $speedguard_fs ) ) {
-					// Include Freemius SDK.
-					require_once dirname(__FILE__) . '/freemius/start.php';
-
-					$speedguard_fs = fs_dynamic_init( array(
-						'id'                  => '15835',
-						'slug'                => 'speedguard',
-						'premium_slug'        => 'speedguard-pro',
-						'type'                => 'plugin',
-						'public_key'          => 'pk_4f087343623f01d0a96151c22d6f9',
-						'is_premium'          => true,
-						'premium_suffix'      => 'PRO',
-						// If your plugin is a serviceware, set this option to false.
-						'has_premium_version' => true,
-						'has_addons'          => false,
-						'has_paid_plans'      => true,
-						'trial'               => array(
-							'days'               => 7,
-							'is_require_payment' => false,
-						),
-						'menu'                => array(
-							'slug'           => 'speedguard_tests',
-							'first-path'     => 'admin.php?page=speedguard_tests',
-						),
-					) );
-				}
-
-				return $speedguard_fs;
-			}
-
-			// Init Freemius.
-			speedguard_fs();
-			// Signal that SDK was initiated.
-			do_action( 'speedguard_fs_loaded' );
-		}
+function activate_speedguard( $network_wide ) {
+	// Network-wide  activation is a PRO feature. If tries to activate Network wide, stop:
+	if ( is_multisite() && $network_wide && ( ! defined( 'SPEEDGUARD_PRO' ) ) ) {
+		deactivate_plugins( plugin_basename( __FILE__ ) );
+		wp_die( esc_html__( 'Network activation is not available at the moment. But feel free to activate this plugin on per-site basis!', 'speedguard' ) );
 	}
 
-	// SpeedGuard main file logic ...
+	// Activate in all other cases
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-speedguard-activator.php';
+	Speedguard_Activator::activate();
+}
 
-	function activate_speedguard( $network_wide ) {
-		// Network-wide  activation is a PRO feature. If tries to activate Network wide, stop:
-		if ( is_multisite() && $network_wide && ( ! defined( 'SPEEDGUARD_PRO' ) ) ) {
-			deactivate_plugins( plugin_basename( __FILE__ ) );
-			wp_die( esc_html__( 'Network activation is not available at the moment. But feel free to activate this plugin on per-site basis!', 'speedguard' ) );
-		}
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-speedguard-deactivator.php
+ */
+function deactivate_speedguard() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-speedguard-deactivator.php';
+	Speedguard_Deactivator::deactivate();
+}
 
-		// Activate in all other cases
-		require_once plugin_dir_path( __FILE__ ) . 'includes/class-speedguard-activator.php';
-		Speedguard_Activator::activate();
-	}
-
-	/**
-	 * The code that runs during plugin deactivation.
-	 * This action is documented in includes/class-speedguard-deactivator.php
-	 */
-	function deactivate_speedguard() {
-		require_once plugin_dir_path( __FILE__ ) . 'includes/class-speedguard-deactivator.php';
-		Speedguard_Deactivator::deactivate();
-	}
-
-	register_activation_hook( __FILE__, 'activate_speedguard' );
-	register_deactivation_hook( __FILE__, 'deactivate_speedguard' );
+register_activation_hook( __FILE__, 'activate_speedguard' );
+register_deactivation_hook( __FILE__, 'deactivate_speedguard' );
 
 
-	register_uninstall_hook(__FILE__, 'uninstall_speedguard');
+register_uninstall_hook( __FILE__, 'uninstall_speedguard' );
 
-	function uninstall_speedguard() {
-		// Uninstall logic here
-		require_once plugin_dir_path( __FILE__ ) . '/admin/class-speedguard-admin.php';
-		require_once plugin_dir_path( __FILE__ ) . '/admin/includes/class.tests-table.php';
+function uninstall_speedguard() {
+	// Uninstall logic here
+	require_once plugin_dir_path( __FILE__ ) . '/admin/class-speedguard-admin.php';
+	require_once plugin_dir_path( __FILE__ ) . '/admin/includes/class.tests-table.php';
 
 // Delete all data
-		function speedguard_delete_data() {
+	function speedguard_delete_data() {
 
-			// Delete CPTs
-			$guarded_pages = get_posts([
-				'post_type'     => ['guarded-page', SpeedGuard_Admin::$cpt_name], // Backwards compatibility
-				'post_status'   => 'any',
-				'posts_per_page' => -1,
-				'fields'        => 'ids',
-				'no_found_rows' => true,
-			]);
+		// Delete CPTs
+		$guarded_pages = get_posts( [
+			'post_type'      => [ 'guarded-page', SpeedGuard_Admin::$cpt_name ], // Backwards compatibility
+			'post_status'    => 'any',
+			'posts_per_page' => - 1,
+			'fields'         => 'ids',
+			'no_found_rows'  => true,
+		] );
 
-			foreach ($guarded_pages as $guarded_page_id) {
-				SpeedGuard_Tests::delete_test_fn($guarded_page_id);
-			}
-
-			// Delete posts meta
-			$guarded_posts = get_posts([
-				'post_type'     => 'any',
-				'post_status'   => 'any',
-				'fields'        => 'ids',
-				'meta_query'    => [
-					'relation' => 'AND',
-					[
-						'key'     => 'speedguard_on',
-						'compare' => 'EXISTS',
-					],
-				],
-				'no_found_rows' => true,
-			]);
-
-			foreach ($guarded_posts as $guarded_post_id) {
-				delete_post_meta($guarded_post_id, 'speedguard_on');
-			}
-
-			// Delete terms meta
-			$the_terms = get_terms([
-				'fields'     => 'ids',
-				'hide_empty' => false,
-				'meta_query' => [
-					[
-						'key'     => 'speedguard_on',
-						'compare' => 'EXISTS',
-					],
-				],
-			]);
-
-			foreach ($the_terms as $term_id) {
-				delete_term_meta($term_id, 'speedguard_on');
-			}
-
-			// Delete options
-			$speedguard_options = [
-				'speedguard_options',
-				'sg_origin_results'
-			];
-			foreach ($speedguard_options as $option_name) {
-				delete_option($option_name);
-				if (is_multisite()) {
-					delete_site_option($option_name);
-				}
-			}
-
-			// Delete non-expiring transients (auto-expiring will be deleted automatically)
-			$speedguard_transients = [
-				'speedguard_tests_in_queue',
-				'speedguard_test_in_progress',
-				'speedguard_sending_request_now',
-				'speedguard_tests_count'
-			];
-			foreach ($speedguard_transients as $speedguard_transient) {
-				delete_transient($speedguard_transient);
-			}
-
-			// Delete CRON jobs
-			wp_clear_scheduled_hook('speedguard_update_results');
-			wp_clear_scheduled_hook('speedguard_email_test_results');
+		foreach ( $guarded_pages as $guarded_page_id ) {
+			SpeedGuard_Tests::delete_test_fn( $guarded_page_id );
 		}
+
+		// Delete posts meta
+		$guarded_posts = get_posts( [
+			'post_type'     => 'any',
+			'post_status'   => 'any',
+			'fields'        => 'ids',
+			'meta_query'    => [
+				'relation' => 'AND',
+				[
+					'key'     => 'speedguard_on',
+					'compare' => 'EXISTS',
+				],
+			],
+			'no_found_rows' => true,
+		] );
+
+		foreach ( $guarded_posts as $guarded_post_id ) {
+			delete_post_meta( $guarded_post_id, 'speedguard_on' );
+		}
+
+		// Delete terms meta
+		$the_terms = get_terms( [
+			'fields'     => 'ids',
+			'hide_empty' => false,
+			'meta_query' => [
+				[
+					'key'     => 'speedguard_on',
+					'compare' => 'EXISTS',
+				],
+			],
+		] );
+
+		foreach ( $the_terms as $term_id ) {
+			delete_term_meta( $term_id, 'speedguard_on' );
+		}
+
+		// Delete options
+		$speedguard_options = [
+			'speedguard_options',
+			'sg_origin_results'
+		];
+		foreach ( $speedguard_options as $option_name ) {
+			delete_option( $option_name );
+			if ( is_multisite() ) {
+				delete_site_option( $option_name );
+			}
+		}
+
+		// Delete non-expiring transients (auto-expiring will be deleted automatically)
+		$speedguard_transients = [
+			'speedguard_tests_in_queue',
+			'speedguard_test_in_progress',
+			'speedguard_sending_request_now',
+			'speedguard_tests_count'
+		];
+		foreach ( $speedguard_transients as $speedguard_transient ) {
+			delete_transient( $speedguard_transient );
+		}
+
+		// Delete CRON jobs
+		wp_clear_scheduled_hook( 'speedguard_update_results' );
+		wp_clear_scheduled_hook( 'speedguard_email_test_results' );
+	}
 
 // Search all blogs if Multisite
-		if ( is_multisite() ) {
-			$sites = get_sites();
-			foreach ( $sites as $site ) {
-				$blog_id = $site->blog_id;
-				switch_to_blog( $blog_id );
-				speedguard_delete_data();
-				restore_current_blog();
-			}
-		} else {
+	if ( is_multisite() ) {
+		$sites = get_sites();
+		foreach ( $sites as $site ) {
+			$blog_id = $site->blog_id;
+			switch_to_blog( $blog_id );
 			speedguard_delete_data();
+			restore_current_blog();
 		}
-
+	} else {
+		speedguard_delete_data();
 	}
 
-	/**
-	 * The core plugin class that is used to define internationalization,
-	 * admin-specific hooks, and public-facing site hooks.
-	 */
-	require plugin_dir_path( __FILE__ ) . 'includes/class-speedguard.php';
-
-	/**
-	 * Begins execution of the plugin.
-	 *
-	 * Since everything within the plugin is registered via hooks,
-	 * then kicking off the plugin from this point in the file does
-	 * not affect the page life cycle.
-	 *
-	 * @since    1.0.0
-	 */
-
-	function run_speedguard() {
-
-		$plugin = new Speedguard();
-		$plugin->run();
-	}
-
-	run_speedguard();
 }
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-speedguard.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+
+function run_speedguard() {
+
+	$plugin = new Speedguard();
+	$plugin->run();
+}
+
+run_speedguard();
+// End of SpeedGuard main file logic ...
 
 
 
