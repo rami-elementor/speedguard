475ad114e8355600ddce31989715a7fbab2674a1
Sabrina Zeidan <sabrinazeidan@gmail.com>
2024-06-26 19:16:20 +0200

    Inifinite request fixed on localhost

--- admin/assets/js/execute_tests.js
+++ admin/assets/js/execute_tests.js
@@ -3,57 +3,93 @@
  */
 
 async function fetchAll(url_to_test) {
-    // What they chose in settings CWV or PSI doesn't matter here -- we retrieve both anyways
     const request_url = 'https://www.googleapis.com/pagespeedonline/v5/runPagespeed?category=performance&url=' + url_to_test + '&';
     const devices = ['mobile', 'desktop'];
+    const defaultValue = 'No data';
+    let singleURLresult = [];
 
     try {
-        singleURLresult = [];
-        let tests = await Promise.all(
-            //separate test for Device, but the same for PSI and CWV
-            devices.map(device => fetch(request_url + 'strategy=' + device)
-                .then(r => r.json())
-                .catch(error => ({error, url}))
-            )
-        )
-        for (let item of tests) {
+        singleURLresult = await Promise.all(devices.map(async device => {
+            try {
+                const response = await fetch(request_url + 'strategy=' + device);
 
-            //get current device value
-            const device = item.lighthouseResult.configSettings.emulatedFormFactor;
-
-            //Data for the Single URL (both CWV and PSI)
-            const defaultValue = 'No data';
-            const URL_RESULTS = {
-                "cwv": {
-                    "lcp": item?.loadingExperience?.metrics?.LARGEST_CONTENTFUL_PAINT_MS ?? defaultValue,
-                    "cls": item?.loadingExperience?.metrics?.CUMULATIVE_LAYOUT_SHIFT_SCORE ?? defaultValue,
-                    "inp": item?.loadingExperience?.metrics?.INTERACTION_TO_NEXT_PAINT ?? defaultValue,
-                    "overall_category": item?.loadingExperience?.overall_category ?? defaultValue
-                },
-                "psi": {
-                    "lcp": item?.lighthouseResult?.audits['largest-contentful-paint'] ?? defaultValue,
-                    "cls": item?.lighthouseResult?.audits['cumulative-layout-shift'] ?? defaultValue
-                },
-                "originCWV": { //Add Data for CWV Origin
-                    "lcp": item?.originLoadingExperience?.metrics?.LARGEST_CONTENTFUL_PAINT_MS ?? defaultValue,
-                    "cls": item?.originLoadingExperience?.metrics?.CUMULATIVE_LAYOUT_SHIFT_SCORE ?? defaultValue,
-                    "inp": item?.originLoadingExperience?.metrics?.INTERACTION_TO_NEXT_PAINT ?? defaultValue,
-                    "overall_category": item?.originLoadingExperience?.overall_category ?? defaultValue
+                if (!response.ok) {
+                    console.error(`HTTP error ${response.status} occurred for ${device}`);
+                    throw new Error(`HTTP error ${response.status}`);
                 }
-            };
 
-            console.log(URL_RESULTS);
-            //Save data to the new object based on device value
-            let singleURLresultperdevice = {
-                [device]: {"psi": URL_RESULTS.psi, "cwv": URL_RESULTS.cwv, "originCWV": URL_RESULTS.originCWV}
-            };
-            singleURLresult.push(singleURLresultperdevice);
-        }
+                const item = await response.json();
 
+                // get current device value
+                const currentDevice = item.lighthouseResult.configSettings.emulatedFormFactor;
 
-        return singleURLresult;
+                // Data for the Single URL (both CWV and PSI)
+                const URL_RESULTS = {
+                    "cwv": {
+                        "lcp": item?.loadingExperience?.metrics?.LARGEST_CONTENTFUL_PAINT_MS ?? defaultValue,
+                        "cls": item?.loadingExperience?.metrics?.CUMULATIVE_LAYOUT_SHIFT_SCORE ?? defaultValue,
+                        "inp": item?.loadingExperience?.metrics?.INTERACTION_TO_NEXT_PAINT ?? defaultValue,
+                        "overall_category": item?.loadingExperience?.overall_category ?? defaultValue
+                    },
+                    "psi": {
+                        "lcp": item?.lighthouseResult?.audits['largest-contentful-paint'] ?? defaultValue,
+                        "cls": item?.lighthouseResult?.audits['cumulative-layout-shift'] ?? defaultValue
+                    },
+                    "originCWV": {
+                        "lcp": item?.originLoadingExperience?.metrics?.LARGEST_CONTENTFUL_PAINT_MS ?? defaultValue,
+                        "cls": item?.originLoadingExperience?.metrics?.CUMULATIVE_LAYOUT_SHIFT_SCORE ?? defaultValue,
+                        "inp": item?.originLoadingExperience?.metrics?.INTERACTION_TO_NEXT_PAINT ?? defaultValue,
+                        "overall_category": item?.originLoadingExperience?.overall_category ?? defaultValue
+                    }
+                };
+
+                // Save data to the new object based on device value
+                let singleURLresultperdevice = {
+                    [currentDevice]: {
+                        "psi": URL_RESULTS.psi,
+                        "cwv": URL_RESULTS.cwv,
+                        "originCWV": URL_RESULTS.originCWV
+                    }
+                };
+
+                return singleURLresultperdevice;
+            } catch (error) {
+                console.error(`An error occurred for ${device}:`, error.message);
+
+                // If there's an error, return object with default values
+                const URL_RESULTS = {
+                    "cwv": {
+                        "lcp": defaultValue,
+                        "cls": defaultValue,
+                        "fid": defaultValue,
+                        "overall_category": defaultValue
+                    },
+                    "psi": {
+                        "lcp": defaultValue,
+                        "cls": defaultValue
+                    },
+                    "originCWV": {
+                        "lcp": defaultValue,
+                        "cls": defaultValue,
+                        "fid": defaultValue,
+                        "overall_category": defaultValue
+                    }
+                };
+
+                // Return object with default values for the current device
+                return {
+                    [device]: {
+                        "psi": URL_RESULTS.psi,
+                        "cwv": URL_RESULTS.cwv,
+                        "originCWV": URL_RESULTS.originCWV
+                    }
+                };
+            }
+        }));
+
     } catch (err) {
-        console.log('There was an errror:');
-        console.log(err)
+        console.error('There was an error:', err);
     }
+
+    return singleURLresult;
 }
