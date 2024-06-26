832ccc0bbd7197e48588970ce85f8e261214d49b
Sabrina Zeidan <sabrinazeidan@gmail.com>
2024-06-26 15:29:21 +0200

    Fix tests not finished on localhost+ check results in 10s

--- admin/assets/js/initiate_tests.js
+++ admin/assets/js/initiate_tests.js
@@ -20,8 +20,8 @@ const check_tests_queue_status = async (ajaxurl, reload) => {
         console.log('Received data:', data);
 
         if (data.status === 'queue') {
-            console.log('Tests are in queue. Checking again in 15 seconds...');
-            setTimeout(() => check_tests_queue_status(ajaxurl, reload), 15000);
+            console.log('Tests are in queue. Checking again in 10 seconds...');
+            setTimeout(() => check_tests_queue_status(ajaxurl, reload), 10000);
             return sg_run_one_test(ajaxurl, data.speedguard_test_in_progress_url, run_nonce, data.speedguard_test_in_progress_id);
         } else if (data.status === 'last_test_complete' && reload === 'true') {
             console.log('Last test completed. Reloading page...');
